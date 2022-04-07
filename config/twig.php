5a19c9a6ea2484b030b453d8598bd7f4e93d45b5
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-04-07 14:21:14 +0200

    heritage&inclusion

--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-$twig = new Twig\Environment($loader, ['debug' => true]);
-$twig->addExtension(new Twig\Extension\DebugExtension());
+//$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+//$twig = new Twig\Environment($loader, ['debug' => true]);
+//$twig->addExtension(new Twig\Extension\DebugExtension());
