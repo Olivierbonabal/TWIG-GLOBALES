4a5d3bc2011753c2aa4829fc15f42ebdd1b27caa
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-04-07 16:35:31 +0200

    globale

--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-//$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-//$twig = new Twig\Environment($loader, ['debug' => true]);
-//$twig->addExtension(new Twig\Extension\DebugExtension());
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Twig\Environment($loader, ['debug' => true]);
+$twig->addExtension(new Twig\Extension\DebugExtension());
+
+$twig->addGlobal('contactEmail', 'bdhfbz@jfbfh.com');
+
+
+
+
+
+
